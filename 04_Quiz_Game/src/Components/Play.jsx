d743221d69a08141bac691eb66e1ac204c85d55c
JbBhetwal <jbjbjbaaa@gmail.com>
2024-01-20 00:08:23 +0545

    Play Game page styling

--- 04_Quiz_Game/src/Components/Play.jsx
+++ 04_Quiz_Game/src/Components/Play.jsx
@@ -3,7 +3,7 @@
 function Play() {
   return (
     <div className=" h-screen bg-gray-200 flex justify-center">
-      <div className="flex flex-col p-8 items-center mt-16 bg-white w-2/5 h-96">
+      <div className="flex flex-col p-8 items-center mt-16 bg-white w-2/5 h-96 rounded-md">
         <div className="bg-rose-500 p-2 rounded-md text-white m-4">
           <p className="font-bold text-2xl text-center">
             Which is the tallest mountain in the world?
@@ -11,24 +11,26 @@ function Play() {
         </div>
         <div className="flex flex-col justify-evenly text-xl p-4 w-full h-full m-4">
           <div className="flex flex-row justify-between">
-            <button className="bg-sky-400 p-2 rounded-md w-1/2 m-2">
-              a.Mt.Everest
+            <button className="bg-sky-400 p-2 rounded-md text-white w-1/2 m-2">
+              Mt.Everest
             </button>
-            <button className="bg-sky-400 p-2 rounded-md w-1/2 m-2">
-              b.Mt.K2
+            <button className="bg-sky-400 p-2 rounded-md text-white w-1/2 m-2">
+              Mt.K2
             </button>
           </div>
           <div className="flex flex-row justify-between">
-            <button className="bg-sky-400 p-2 rounded-md w-1/2 m-2">
-              c.Mt.Makalu
+            <button className="bg-sky-400 p-2 rounded-md text-white w-1/2 m-2">
+              Mt.Makalu
             </button>
-            <button className="bg-sky-400 p-2 rounded-md w-1/2 m-2">
-              d.Mt.Fuji
+            <button className="bg-sky-400 p-2 rounded-md text-white w-1/2 m-2">
+              Mt.Fuji
             </button>
           </div>
         </div>
         <div>
-          <button className="bg-orange-400 p-2 rounded-md m-2">Next</button>
+          <button className="bg-orange-400 px-4 py-2 text-white font-bold text-xl rounded-md">
+            Next
+          </button>
         </div>
       </div>
     </div>
