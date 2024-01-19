71994cac9c5ab8a3845e575553d41025d2967025
JbBhetwal <jbjbjbaaa@gmail.com>
2024-01-20 00:23:46 +0545

    Log in page styling

--- 03_React_Router/src/components/LogIn.jsx
+++ 03_React_Router/src/components/LogIn.jsx
@@ -2,8 +2,38 @@ import React from "react";
 
 function LogIn() {
   return (
-    <div>
-      <img src="https://d33v4339jhl8k0.cloudfront.net/docs/assets/524448053e3e9bd67a3dc68a/images/654e45ca8fd0677319115e88/file-D5YpTaTMre.png" />
+    <div className="flex items-center justify-center">
+      <form className="flex flex-col h-96 w-96 p-8 m-2 bg-gray-200 rounded-md  align-center">
+        <div className="flex flex-col m-2">
+          <label>Enter username:</label>
+          <input
+            required
+            type="text"
+            className="p-2 border rounded-md border-black"
+          />
+        </div>
+        <div className="flex flex-col m-2">
+          <label>Enter email id:</label>
+          <input
+            required
+            type="email"
+            className="p-2 border rounded-md border-black"
+          />
+        </div>
+        <div className="flex flex-col m-2">
+          <label>Enter password:</label>
+          <input
+            required
+            type="password"
+            className="p-2 border rounded-md border-black"
+          />
+        </div>
+        <div className="flex justify-center m-4">
+          <button className="bg-sky-500 p-2 text-white font-bold rounded-md">
+            Log in
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
